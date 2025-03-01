916b1b0ddf3918b5bfa4c58dc1ad56ea9a475ee8
insertcoolgithubname <68465536+insertcoolgithubname@users.noreply.github.com>
2025-03-01 15:10:51 +0100

    add main fun

--- ocr-to-latex.py
+++ ocr-to-latex.py
@@ -3,21 +3,26 @@ from PIL import ImageGrab
 from surya.texify import TexifyPredictor
 import clipboard as cb
 
-image = ImageGrab.grabclipboard()
-# image = Image.open("C:/Users/petr/source/repos/surya/test.png")
-predictor = TexifyPredictor()
 
-if image is not None:
-    prediction = (predictor([image]))
-    latex_text = prediction[0].text
+def main():
+    image = ImageGrab.grabclipboard()
+    predictor = TexifyPredictor()
 
-    # remove start and end of math segment to allow pasting into desmos
-    latex_text = latex_text.replace('<math display="block">', "")
-    latex_text = latex_text.replace('</math>', "")
-    # remove bold symbols
-    latex_text = latex_text.replace('\\mathbf', "")
+    if image is not None:
+        prediction = (predictor([image]))
+        latex_text = prediction[0].text
 
-    print(latex_text)
-    cb.copy(latex_text)
-else:
-    sys.exit("clipboard has no image")
+        # remove start and end of math segment to allow pasting into desmos
+        latex_text = latex_text.replace('<math display="block">', "")
+        latex_text = latex_text.replace('</math>', "")
+        # remove bold symbols
+        latex_text = latex_text.replace('\\mathbf', "")
+
+        print(latex_text)
+        cb.copy(latex_text)
+    else:
+        sys.exit("clipboard has no image")
+
+
+if __name__ == "__main__":
+    main()
